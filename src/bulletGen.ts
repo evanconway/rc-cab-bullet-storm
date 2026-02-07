86f6c075b11d2dc94132411d8e96274572ac9958
Evan Conway <evanconwaymusic@gmail.com>
2026-02-07 17:34:07 -0500

    add crude bullet gen

---
+++ src/bulletGen.ts
@@ -0,0 +1,131 @@
+import { SCREEN } from "./canvas";
+
+const SCREEN_BUFFER = 30; // num of pixels to gen and delete bullets offscreen
+const SCREEN_BUFFER_LEFT = SCREEN_BUFFER * -1;
+const SCREEN_BUFFER_RIGHT = SCREEN.WIDTH + SCREEN_BUFFER;
+const SCREEN_BUFFER_TOP = SCREEN_BUFFER * -1;
+const SCREEN_BUFFER_BOTTOM = SCREEN.HEIGHT + SCREEN_BUFFER;
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface BulletSimple {
+  position: Position;
+  velocity: { x: number; y: number };
+}
+
+class BulletGen {
+  private bulletId: number;
+  private bullets: Map<number, BulletSimple>;
+  private bulletsHaveEnteredScreen: Set<number>;
+
+  constructor() {
+    this.bulletId = 0;
+    this.bullets = new Map();
+    this.bulletsHaveEnteredScreen = new Set();
+  }
+
+  private addBullet(bullet: BulletSimple) {
+    const newId = this.bulletId;
+    this.bulletId += 1;
+    this.bullets.set(newId, bullet);
+  }
+
+  private removeBullet(id: number) {
+    this.bullets.delete(id);
+  }
+
+  /**
+   * 0 is left, 1 is top, 2 is right, 3 is bottom
+   */
+  private getRandomScreenEdge() {
+    const edge = Math.floor(Math.random() * 4);
+    if (edge === 1) return 1;
+    if (edge == 2) return 2;
+    if (edge === 3) return 3;
+    return 0;
+  }
+
+  private positionIsOnScreen({ x, y }: Position) {
+    return x >= 0 && x <= SCREEN.WIDTH && y >= 0 && y <= SCREEN.HEIGHT;
+  }
+
+  private positionIsOutsideScreenBuffer({ x, y }: Position) {
+    return (
+      x < SCREEN_BUFFER_LEFT ||
+      x > SCREEN_BUFFER_RIGHT ||
+      y < SCREEN_BUFFER_TOP ||
+      y > SCREEN_BUFFER_BOTTOM
+    );
+  }
+
+  getBulletCount() {
+    return this.bullets.size;
+  }
+
+  update(updatePercent: number) {
+    this.bullets.forEach((bullet, id) => {
+      bullet.position.x += bullet.velocity.x * updatePercent;
+      bullet.position.y += bullet.velocity.y * updatePercent;
+
+      if (this.positionIsOnScreen(bullet.position)) {
+        this.bulletsHaveEnteredScreen.add(id);
+      }
+
+      if (this.positionIsOutsideScreenBuffer(bullet.position)) {
+        this.removeBullet(id);
+      }
+    });
+  }
+
+  draw(context: CanvasRenderingContext2D) {
+    context.strokeStyle = "#f00";
+    this.bullets.forEach((bullet) => {
+      context.beginPath();
+      context.arc(
+        bullet.position.x,
+        bullet.position.y,
+        5,
+        0,
+        Math.PI * 2,
+        true,
+      );
+      context.stroke();
+    });
+  }
+
+  genSimpleRandomEdgeBullet() {
+    const newBullet: BulletSimple = {
+      position: { x: 0, y: 0 },
+      velocity: { x: 0, y: 0 },
+    };
+    const speed = 2;
+    const edge = this.getRandomScreenEdge();
+    if (edge === 0) {
+      // left
+      newBullet.position.x = SCREEN_BUFFER * -1;
+      newBullet.position.y = Math.random() * SCREEN.HEIGHT;
+      newBullet.velocity.x = speed;
+    } else if (edge === 1) {
+      // top
+      newBullet.position.x = Math.random() * SCREEN.WIDTH;
+      newBullet.position.y = SCREEN_BUFFER * -1;
+      newBullet.velocity.y = speed;
+    } else if (edge === 2) {
+      // right
+      newBullet.position.x = SCREEN.WIDTH + SCREEN_BUFFER;
+      newBullet.position.y = Math.random() * SCREEN.HEIGHT;
+      newBullet.velocity.x = speed * -1;
+    } else if (edge === 3) {
+      // bottom
+      newBullet.position.x = Math.random() * SCREEN.WIDTH;
+      newBullet.position.y = SCREEN.HEIGHT + SCREEN_BUFFER;
+      newBullet.velocity.y = speed * -1;
+    }
+    this.addBullet(newBullet);
+  }
+}
+
+export default BulletGen;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,45 @@
 import "./style.css";
 import { PLAYER_1, SYSTEM } from "@rcade/plugin-input-classic";
 import { setGameLoop, SCREEN } from "./canvas";
+import BulletGen from "./bulletGen";
+
+const bulletManager = new BulletGen();
 
 const position = { x: SCREEN.WIDTH / 2, y: SCREEN.HEIGHT / 2 };
 
+const drawPlayerBox = (context: CanvasRenderingContext2D) => {
+  context.fillStyle = "#fff";
+  const playerSize = 10;
+  context.fillRect(
+    Math.floor(position.x - playerSize / 2),
+    Math.floor(position.y - playerSize / 2),
+    10,
+    10,
+  );
+};
+
+const BULLET_GEN_INTERVAL = 5;
+let bulletGenTime = 0;
+
 setGameLoop(({ context, getFrameTimeNormalizedNum }) => {
-  const speed = getFrameTimeNormalizedNum(Math.sqrt(2));
+  const speed = getFrameTimeNormalizedNum(Math.sqrt(8));
 
   if (PLAYER_1.DPAD.up) position.y -= speed;
   if (PLAYER_1.DPAD.down) position.y += speed;
   if (PLAYER_1.DPAD.left) position.x -= speed;
   if (PLAYER_1.DPAD.right) position.x += speed;
 
-  context.fillStyle = "#fff";
-  context.fillText("hello world", 8, 16);
+  bulletGenTime += getFrameTimeNormalizedNum(1);
+  if (bulletGenTime >= BULLET_GEN_INTERVAL) {
+    bulletManager.genSimpleRandomEdgeBullet();
+    bulletGenTime = 0;
+  }
 
-  context.fillRect(Math.floor(position.x), Math.floor(position.y), 10, 10);
+  bulletManager.update(getFrameTimeNormalizedNum(1));
+  bulletManager.draw(context);
+
+  context.fillStyle = "#fff";
+  context.fillText(`bullets: ${bulletManager.getBulletCount()}`, 8, 16);
+
+  drawPlayerBox(context);
 });
